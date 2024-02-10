a10ff637ae49a673bd188293cc058240c1f80f5d
Shamie J <shamiejegan@gmail.com>
2024-02-11 01:14:59 +0800

    improved introduction to autism, and mechanism for transitioning to next scene

--- Assets/Scripts/Intro/ASDIntroController.cs
+++ Assets/Scripts/Intro/ASDIntroController.cs
@@ -1,28 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class ASDIntroController : MonoBehaviour
 {
-    [SerializeField] float showHeartTimer;
-    [SerializeField] GameObject diverseLine;
+    [SerializeField] GameObject diverseLineHorizontal;
+    [SerializeField] GameObject diverseLineVertical;
     [SerializeField] float diverseLineTimer;
     [SerializeField] GameObject asdNPC;
     [SerializeField] float npcTimer;
-    [SerializeField] private GameObject animated_heart; 
-    [SerializeField] float heartTimer;
+    [SerializeField] float readyTimer;
+    [SerializeField] GameObject surroundParticles; 
+    [SerializeField] GameObject leftController;
+    [SerializeField] GameObject rightController;
 
 
     //when gameobject is active
     public void Start()
     {
-        diverseLine.SetActive(false);
+        diverseLineHorizontal.SetActive(false);
+        diverseLineVertical.SetActive(false);
         asdNPC.SetActive(false);
-        animated_heart.SetActive(false);
+        //disable collider by before end of explanation 
+        asdNPC.GetComponent<CapsuleCollider>().enabled = false;
+
+        surroundParticles.SetActive(false);
+
         GetComponent<AudioSource>().Play();
         StartCoroutine(showNPC());
         StartCoroutine(showDiverseLine());
-        StartCoroutine(showHeart());
+        StartCoroutine(finalIntroAudio());
+        StartCoroutine(makeNPCSelectable());
+
     }
 
     public IEnumerator showNPC(){
@@ -32,14 +44,30 @@ public class ASDIntroController : MonoBehaviour
 
     public IEnumerator showDiverseLine(){
         yield return new WaitForSeconds(diverseLineTimer);
-        diverseLine.SetActive(true);
+        diverseLineHorizontal.SetActive(true);
+        diverseLineVertical.SetActive(true);
     }
 
 
-    public IEnumerator showHeart()
+    public IEnumerator finalIntroAudio()
     {
-        yield return new WaitForSeconds(heartTimer);
-        animated_heart.SetActive(true);
+        yield return new WaitForSeconds(readyTimer);
+        // //make NPC's XR Simple Interactable component enabled
+        asdNPC.GetComponent<AudioSource>().Play();
+    }
+
+    public IEnumerator makeNPCSelectable()
+    {
+        yield return new WaitForSeconds(readyTimer+4.0f);
+        asdNPC.GetComponent<CapsuleCollider>().enabled = true;
+    
+    }
+
+    public void StartASDSimulation()
+    {
+        Debug.Log("StartASDSimulation");
+        //change scene to the next scene
+        SceneManager.LoadScene("Simulation");
     }
 
 }
